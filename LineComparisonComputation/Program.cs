b87b513539844961d235c01d80da32876a8d5964
NikitaSalunke10 <salunkenikita10@gmail.com>
2022-07-16 14:49:10 +0530

    UC-1 calculating the length

--- LineComparisonComputation/Program.cs
+++ LineComparisonComputation/Program.cs
@@ -1,1 +1,4 @@
-﻿Console.WriteLine("Welcome to Line Comparison Computation Program");
+﻿double x1 = 3, x2 = 4, y1 = 4, y2 =3;
+//double length = Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+double length = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+Console.WriteLine("The length is : " +length);

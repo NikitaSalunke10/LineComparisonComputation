3a8463e96940e1bc80a0c855be18ab7f75d62499
NikitaSalunke10 <salunkenikita10@gmail.com>
2022-07-16 14:59:21 +0530

    Check equality of two lengths

--- LineComparisonComputation/Program.cs
+++ LineComparisonComputation/Program.cs
@@ -1,4 +1,5 @@
 ﻿double x1 = 3, x2 = 4, y1 = 4, y2 =3;
-//double length = Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
-double length = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
-Console.WriteLine("The length is : " +length);
+double x3 = 3, x4 = 4, y3 = 4, y4 = 3;
+double line1 = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+double line2 = Math.Sqrt(Math.Pow(x4 - x3, 2) + Math.Pow(y4 - y3, 2));
+Console.WriteLine(line1.Equals(line2));

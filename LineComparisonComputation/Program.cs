d83b1858c585720dc93d8a73331e9e8668041a8a
NikitaSalunke10 <salunkenikita10@gmail.com>
2022-07-17 20:27:15 +0530

    equals method used to check equality

--- LineComparisonComputation/Program.cs
+++ LineComparisonComputation/Program.cs
@@ -1,5 +1,5 @@
-﻿double x1 = 3, x2 = 4, y1 = 4, y2 =3;
-double x3 = 3, x4 = 4, y3 = 4, y4 = 3;
+﻿double x1 = 3, x2 = 4, y1 = 6, y2 =3;
+double x3 = 2, x4 = 7, y3 = 3, y4 = 5;
 double line1 = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
 double line2 = Math.Sqrt(Math.Pow(x4 - x3, 2) + Math.Pow(y4 - y3, 2));
 Console.WriteLine(line1.Equals(line2));

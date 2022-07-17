6d949574a318af4cd27426c8697f4c4d8791aff8
NikitaSalunke10 <salunkenikita10@gmail.com>
2022-07-17 20:23:17 +0530

    calculating the length

--- LineComparisonComputation/Program.cs
+++ LineComparisonComputation/Program.cs
@@ -1,4 +1,3 @@
-﻿double x1 = 3, x2 = 4, y1 = 4, y2 =3;
-//double length = Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+﻿double x1 = 2, x2 = 7, y1 = 5, y2 =8;
 double length = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
 Console.WriteLine("The length is : " +length);

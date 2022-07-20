4b8a446134a8be4a1361eef6f5e350c6345e913b
NikitaSalunke10 <salunkenikita10@gmail.com>
2022-07-20 21:45:45 +0530

    Comments added in FindLength program

--- LineComparisonComputation/Program.cs
+++ LineComparisonComputation/Program.cs
@@ -1,3 +1,3 @@
-﻿double x1 = 2, x2 = 7, y1 = 5, y2 =8;
-double length = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
-Console.WriteLine("The length is : " +length);
+﻿double x1 = 2, x2 = 7, y1 = 5, y2 =8; // declared and assign values for x1, x2 and y1, y2
+double length = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2)); // using the formula length is calculated
+Console.WriteLine("The length is : " +length); // displaying the calculated length

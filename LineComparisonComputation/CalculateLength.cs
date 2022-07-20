64cde68e4d6b6a16a59122e9397ed68dcc753ae4
NikitaSalunke10 <salunkenikita10@gmail.com>
2022-07-20 22:47:19 +0530

    EqualsCompareToMethod program added

---
+++ LineComparisonComputation/CalculateLength.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparisonComputation
+{
+    internal class CalculateLength
+    {
+        public double Length(int x1, int x2, int y1, int y2)
+        {
+            double line = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));  // calculated the length for line1 i.e for points x1, x2 and y1, y2 
+            return line;
+        }
+    }
+}

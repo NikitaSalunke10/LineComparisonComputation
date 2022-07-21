b06ce338ecd5259746cb6536b7162253fe25f851
NikitaSalunke10 <salunkenikita10@gmail.com>
2022-07-21 13:48:53 +0530

    Equals and Compareto methods used in program

--- LineComparisonComputation/CalculateLength.cs
+++ LineComparisonComputation/CalculateLength.cs
@@ -8,8 +8,13 @@ namespace LineComparisonComputation
 {
     internal class CalculateLength
     {
-        public double Length(int x1, int x2, int y1, int y2)
+        public double Length()
         {
+            Console.WriteLine("Enter the points for line: ");
+            int x1 = int.Parse(Console.ReadLine());
+            int x2 = int.Parse(Console.ReadLine());
+            int y1 = int.Parse(Console.ReadLine());
+            int y2 = int.Parse(Console.ReadLine());
             double line = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));  // calculated the length for line1 i.e for points x1, x2 and y1, y2 
             return line;
         }
--- LineComparisonComputation/Program.cs
+++ LineComparisonComputation/Program.cs
@@ -1,10 +1,11 @@
 using LineComparisonComputation;
 
-CalculateLength line1 = new CalculateLength();
-CalculateLength line2 = new CalculateLength();
-Console.WriteLine(line1.Equals(line2));
-//int ans = line1.CompareTo(line2);
-//Console.WriteLine(line1.CompareTo(line2)); // using CompareTo method we are comparing 2 lines
+CalculateLength line1 = new CalculateLength(); //object is created 
+CalculateLength line2 = new CalculateLength(); //object is created 
+double length1 = line1.Length(); // calling the function and storing the returned value 
+double length2 = line2.Length(); //calling the function again with different object and storing the returned value 
+Console.WriteLine("The Equals method ans is : "+length1.Equals(length2)); // using Equals method we are checking the equality of two lines and returns true if both length are same or else false
+Console.WriteLine("The CompareTo method ans is : "+length1.CompareTo(length2)); // using CompareTo method we are comparing 2 lines
                                            // if line 1 is less than line 2 then -1 value is printed
                                            // if line 1 is greater than line 2 then 1 value is printed
                                            // if line 1 is equals to line 2 then 0 value is printed
